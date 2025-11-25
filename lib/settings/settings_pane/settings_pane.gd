43efc2935e0fb64f6f74b8f4ceb1c72f694926f0
Alec <alex.laing@me.com>
2025-11-25 18:42:15 +1300

    Visual improvements

--- lib/settings/settings_pane/settings_pane.gd
+++ lib/settings/settings_pane/settings_pane.gd
@@ -2,8 +2,5 @@
 extends UIPane
 
 func _on_apply_pressed() -> void:
-	for _n in $Box.get_children():
-		if _n is Option:
-			print(_n.id + ": " + _n.current_option.id)
 	SettingsHandler.apply_queued_changes()
 	close()
