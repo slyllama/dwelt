35e31f0fc2a32cd3f9a51cfaa7af2791156bd2d4
Alex Laing <alex.laing@me.com>
2026-03-10 12:36:53 +1300

    Camera zooming

---
+++ lib/player/orbit.gd
@@ -0,0 +1,17 @@
+extends Marker3D
+
+@export var zoom_increment := 0.5
+@export var zoom_min := 5.0
+@export var zoom_max := 10.0
+@onready var target_zoom: float = $Camera.position.z
+
+func _input(_event: InputEvent) -> void:
+	if Input.is_action_just_pressed("zoom_in"):
+		target_zoom -= zoom_increment
+	elif Input.is_action_just_pressed("zoom_out"):
+		target_zoom += zoom_increment
+
+func _physics_process(_delta: float) -> void:
+	target_zoom = clamp(target_zoom, zoom_min, zoom_max)
+	$Camera.position.z = lerp(
+		$Camera.position.z, target_zoom, Utils.crit_plerp(6.0))
--- lib/player/player.gd
+++ lib/player/player.gd
@@ -1,9 +1,9 @@
 extends CharacterBody3D
 
 @export_category("Physics")
-@export var speed := 3.0
+@export var speed := 3.4
 @export var speed_multiplier := 1.0 # used for lethargy etc
-@export var friction := 15.0
+@export var friction := 16.0
 @export var gravity_damping := 10.0
 
 var _target_velocity := Vector3.ZERO
