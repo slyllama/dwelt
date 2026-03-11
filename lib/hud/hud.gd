bcb6c78ec023a3c57ebcfaee24a5010ad2e38696
Alec <alex.laing@me.com>
2026-03-12 00:27:34 +1300

    Adjust UI library structure

--- lib/hud/hud.gd
+++
@@ -1,6 +0,0 @@
-extends CanvasLayer
-
-func _ready() -> void:
-	$DebugBG.queue_free()
-	await get_tree().create_timer(0.1).timeout
-	$EyesAnim.animate()
---
+++ lib/ui/hud/hud.gd
@@ -0,0 +1,6 @@
+extends CanvasLayer
+
+func _ready() -> void:
+	$DebugBG.queue_free()
+	await get_tree().create_timer(0.1).timeout
+	%EyesAnim.animate()
