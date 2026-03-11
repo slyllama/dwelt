95f181cd5a80e840d01a179284e876bd7ad5ab83
Alex Laing <alex.laing@me.com>
2026-03-11 16:06:21 +1300

    Improve multiple pane handling

--- lib/ui_pane/ui_pane.gd
+++ lib/ui_pane/ui_pane.gd
@@ -1,13 +1,28 @@
-class_name UIPane extends Panel
+@tool
+class_name UIPane extends PanelContainer
 # UI pane - generic window class
+
+@export var title := "((UIPane))":
+	get: return(title)
+	set(_title):
+		title = _title
+		%Title.text = title
 
 const PADDING := 30.0 # pane can't be carried outside of this padding around the window
 var cursor_last_in_window := true
+var dragging := false
+
+signal clicked
 
 func _on_header_gui_input(event: InputEvent) -> void:
+	if Engine.is_editor_hint(): return
+	
 	# Handle window dragging, including stopping dragging if the cursor gets
 	# too close to the game window, and recovering once it re-enters
 	if Input.is_action_pressed("left_click"):
+		# only drag the pane if the operation was started within the pane's
+		# header
+		if !dragging: return
 		if (!cursor_last_in_window
 			and Utils.cursor_in_window(PADDING)):
 			position = get_window().get_mouse_position()
@@ -15,3 +30,14 @@ func _on_header_gui_input(event: InputEvent) -> void:
 			and Utils.cursor_in_window(PADDING)):
 			position += event.relative
 		cursor_last_in_window = Utils.cursor_in_window(PADDING)
+
+func _on_gui_input(_event: InputEvent) -> void:
+	if Engine.is_editor_hint(): return
+	
+	# Handle dragging initiation and ending
+	if Input.is_action_just_pressed("left_click"):
+		dragging = true
+		Utils.pdebug("Clicked pane " + str(name))
+		clicked.emit()
+	elif Input.is_action_just_released("left_click"):
+		dragging = false
--- lib/ui_pane/ui_pane_manager/ui_pane_manager.gd
+++ lib/ui_pane/ui_pane_manager/ui_pane_manager.gd
@@ -3,13 +3,26 @@ extends CanvasLayer
 
 var panes: Array[UIPane] = []
 
-func update_z_index() -> void:
+func update_draw_order() -> void:
 	for pane in panes:
-		pane.z_index = panes.find(pane)
-		Utils.pdebug(str(panes.find(pane)))
+		pane.move_to_front.call_deferred()
+
+func put_on_top(pane: UIPane) -> void:
+	if pane in panes:
+		panes.erase(pane)
+	panes.push_back(pane)
+	update_draw_order()
 
 func _ready() -> void:
 	for child in get_children():
 		if child is UIPane:
 			panes.append(child)
-	update_z_index()
+			child.clicked.connect(func() -> void:
+				put_on_top(child))
+	update_draw_order()
+
+# TODO: needs testing
+func _on_child_entered_tree(node: Node) -> void:
+	if node is UIPane:
+		panes.push_back(node)
+		update_draw_order()
