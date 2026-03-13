4785b927ca84d760d22953d8d688082f67f2e9db
Alec <alex.laing@me.com>
2026-03-13 19:41:28 +1300

    Implement effects tracking

---
+++ lib/effect/effect_manager/effect_manager.gd
@@ -0,0 +1,39 @@
+class_name EffectManager extends Node
+# The EffectManager is responsible for tracking a gadget's active effects, and
+# their durations and quantities:
+#
+# <effect ID>: {
+#	"qty": <quantity - optional>,
+#	"time_left": <remaining time - optional>
+# }
+
+const TICK := 0.1
+
+var effects := {
+	"test_effect": { "time_left": "5.0" },
+	"qty_effect": { "qty": "10" }
+}
+
+var _t := 0.0 # time
+
+func add_time_effect(id: String, time_left: float) -> void:
+	effects[id] = { "time_left": str(snapped(time_left, TICK)) }
+
+func _ready() -> void:
+	Utils.debug_sent.connect(func(string: String) -> void:
+		if string == "/te":
+			add_time_effect("test_effect", 8.0))
+
+func _process(delta: float) -> void:
+	if _t > TICK:
+		_t = 0
+		# Decrement effect times if they have that property
+		for _e: String in effects:
+			var effect: Dictionary = effects[_e]
+			if "time_left" in effect:
+				var time_left: float = effect.time_left.to_float()
+				if time_left > 0.0:
+					time_left -= TICK
+					effect.time_left = str(snapped(time_left, 0.1))
+				else: effects.erase(_e)
+	_t += delta
