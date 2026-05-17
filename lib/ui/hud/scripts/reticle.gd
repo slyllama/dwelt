f1b7f11210b2ce16dc95009c90d4e0d74e63bc3e
Alex Laing <alex.laing@me.com>
2026-05-18 09:18:47 +1200

    Fix 'reticle' spelling!

--- lib/ui/hud/scripts/reticle.gd
+++ lib/ui/hud/scripts/reticle.gd
@@ -1,6 +1,8 @@
 extends TextureRect
 
 func _ready() -> void:
+	# Toggle reticle visiblity depending on whether the controller is being
+	# used or not
 	DweltInput.current_device_changed.connect(func() -> void:
 		if DweltInput.current_device_mode == DweltInput.DeviceModes.CONTROLLER:
 			visible = true
