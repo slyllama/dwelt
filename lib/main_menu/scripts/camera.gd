1bc56fd39c96844551d19b7736eb200e059b8877
Alec <alex.laing@me.com>
2026-07-25 01:30:46 +1200

    Additional main menu art polish

--- lib/main_menu/scripts/camera.gd
+++ lib/main_menu/scripts/camera.gd
@@ -6,7 +6,7 @@ extends Camera3D
 @onready var original_position := global_position
 
 var mouse_in_window := true
-var _last_mouse_delta := Vector2.ZERO
+var last_mouse_delta := Vector2.ZERO
 
 func _physics_process(_delta: float) -> void:
 	var _mouse_pos := get_window().get_mouse_position()
@@ -17,7 +17,7 @@ func _physics_process(_delta: float) -> void:
 	if mouse_in_window:
 		_mouse_delta = _mouse_pos / _window_size - Vector2(0.5, 0.5)
 	else:
-		_mouse_delta = _last_mouse_delta # don't update if the mouse isn't in the window
+		_mouse_delta = last_mouse_delta # don't update if the mouse isn't in the window
 	_mouse_delta = clamp(_mouse_delta, Vector2(-0.5, -0.5), Vector2(0.5, 0.5))
 	var _translated_delta := Vector3(
 		-_mouse_delta.x * intensity.x,
@@ -25,8 +25,6 @@ func _physics_process(_delta: float) -> void:
 	
 	global_position = lerp(global_position,
 		original_position + _translated_delta, DwUtils.crit_plerp(5.0))
-	
-	_last_mouse_delta = _mouse_delta
 
 func _notification(what: int) -> void:
 	# Detect whether the mouse is in the window
--- lib/main_menu/scripts/world.gd
+++ lib/main_menu/scripts/world.gd
@@ -1,11 +1,14 @@
 extends Node3D
 
+var player_model: Node3D
+
 func _on_player_async_loaded() -> void:
 	%FadePlayerIn.play("fade_in")
-	var player_model: Node3D = $PlayerAsync.add_scene()
+	player_model = $PlayerAsync.add_scene()
 	await player_model.ready
 	
 	# Copy this async node's position and rotation to the resulting mesh
 	player_model.global_position = $PlayerAsync.global_position
 	player_model.global_rotation = $PlayerAsync.global_rotation
+	
 	$PlayerAsync.close()
