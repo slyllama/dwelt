27123fd6f87e225d73d8bab7a129d22ab87e3338
Alec <alex.laing@me.com>
2026-08-03 19:38:14 +1200

    Improve gadget plate
    - Add grass decal

--- lib/effect/effect_card/effect_card.gd
+++ lib/effect/effect_card/effect_card.gd
@@ -1,12 +1,16 @@
 class_name EffectCard extends TextureRect
 # Display an icon for a single effect
 
+const PLACEHOLDER_TEXTURE := preload("res://data/effects/placeholder.jpg")
+
 @export var effect_instance: EffectInstance:
 	set(_effect_instance):
 		effect_instance = _effect_instance
 		if effect_instance:
 			effect_instance.finished.connect(queue_free)
+			
 			if effect_instance.icon: texture = effect_instance.icon
+			else: texture = PLACEHOLDER_TEXTURE
 
 func _process(_delta: float) -> void:
 	if effect_instance:
--- lib/hud/gadget_plate/gadget_plate.gd
+++ lib/hud/gadget_plate/gadget_plate.gd
@@ -4,12 +4,14 @@ func update(gadget: Gadget) -> void:
 	if gadget:
 		%Title.text = gadget.gadget_title
 		if gadget.effect_manager:
+			if !%EffectBar.effect_manager:
+				%Anim.play("fade_in")
 			%EffectBar.effect_manager = gadget.effect_manager
-			%Anim.play("fade_in")
 		else: %EffectBar.effect_manager = null
 	else:
 		%EffectBar.effect_manager = null
-		%Anim.play_backwards("fade_in")
+		if modulate.a > 0.99:
+			%Anim.play_backwards("fade_in")
 
 func _ready() -> void:
 	DwGadget.selected_gadget_changed.connect(update)
