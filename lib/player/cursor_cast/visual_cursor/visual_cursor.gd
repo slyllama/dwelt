438a731b1e8eea9b3403b929a3489813b980acc5
Alec <alex.laing@me.com>
2026-08-07 19:11:07 +1200

    Remove visual cursor (for now)
    Not quite working

--- lib/player/cursor_cast/visual_cursor/visual_cursor.gd
+++
@@ -1,9 +0,0 @@
-extends Node3D
-# Visual indication of position of 3D cursor, mainly for selecting gadgets
-
-func _ready() -> void:
-	visible = false
-	DwGlobal.tool_mode_changed.connect(func(new_tool_mode: DwGlobal.ToolMode) -> void:
-		if new_tool_mode == DwGlobal.ToolMode.SELECT:
-			visible = true
-		else: visible = false)
