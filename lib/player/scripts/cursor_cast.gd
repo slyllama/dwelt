4569bbd9038482737dbfe44993918baa9b62dd50
Alec <alex.laing@me.com>
2026-08-07 18:24:45 +1200

    Extend 3D cursor flexibility

--- lib/player/scripts/cursor_cast.gd
+++ lib/player/scripts/cursor_cast.gd
@@ -1,8 +1,11 @@
-extends Node
+class_name CursorCast extends Marker3D
 # cursor_cast
 # Handles raycasting from the camera through the mouse cursor
 
-@onready var camera: Camera3D = get_parent()
+@export_flags_3d_physics var collision_mask: int
+@export var camera: Camera3D
+@export var handles_gadget_hovering := false
+@export var detection_length := 200.0
 
 var current_collider: CollisionObject3D
 var last_click_in_ui := false
@@ -15,17 +18,19 @@ var _cooldown := 0.0
 func handle_mouse_raycast() -> void:
 	var mouse_pos := get_viewport().get_mouse_position()
 	var _from := camera.project_ray_origin(mouse_pos)
-	var _to := _from + camera.project_ray_normal(mouse_pos) * 200.0
+	var _to := _from + camera.project_ray_normal(mouse_pos) * detection_length
 	var space_state := camera.get_world_3d().direct_space_state
 	var query := PhysicsRayQueryParameters3D.create(_from, _to)
 	
 	query.collide_with_areas = true
-	query.collision_mask = 100
+	query.collision_mask = collision_mask
 	
 	var intersection := space_state.intersect_ray(query)
 	if intersection:
 		current_collider = intersection.collider
-	else: current_collider = null
+		global_position = intersection.position
+	else:
+		current_collider = null
 
 func handle_mouse_release() -> void:
 	if !last_click_in_ui and !get_window().gui_get_hovered_control():
@@ -38,9 +43,12 @@ func handle_gadget_hover() -> void:
 		DwGadget.gadget_hovered.emit(current_collider)
 	last_hovered_gadget = current_collider
 
+func _init() -> void:
+	top_level = true
+
 # Only perform when a valid input event is happening
 func _input(_event: InputEvent) -> void:
-	if !Input.mouse_mode == Input.MOUSE_MODE_VISIBLE:
+	if handles_gadget_hovering and !Input.mouse_mode == Input.MOUSE_MODE_VISIBLE:
 		# Cancel any hovered gadgets when panning events start
 		DwGadget.gadget_hovered.emit(null)
 		return # don't check during panning events
@@ -55,11 +63,17 @@ func _input(_event: InputEvent) -> void:
 func _process(delta: float) -> void:
 	handle_mouse_raycast()
 	if (Input.mouse_mode == Input.MOUSE_MODE_VISIBLE
-		and !get_window().gui_get_hovered_control()):
+		and !get_window().gui_get_hovered_control()
+		and handles_gadget_hovering):
 		handle_gadget_hover()
 	else:
-		last_hovered_gadget = null
-		DwGadget.gadget_hovered.emit(null)
+		if handles_gadget_hovering:
+			last_hovered_gadget = null
+			DwGadget.gadget_hovered.emit(null)
+	
+	# Handle visibility
+	if Input.mouse_mode == Input.MOUSE_MODE_VISIBLE: visible = true
+	else: visible = false
 	
 	if _cooldown > 0.0:
 		_cooldown -= delta
