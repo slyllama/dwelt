37789082ca3ae051763ba3fff23ca5abdbb785e2
Alec <alex.laing@me.com>
2026-08-11 01:47:44 +1200

    Small improvements

--- lib/hud/scripts/skill_name_plate.gd
+++ lib/hud/scripts/skill_name_plate.gd
@@ -3,6 +3,12 @@ extends TextureRect
 func _init() -> void:
 	visible = false
 
+func _ready() -> void:
+	DwGlobal.skill_used.connect(func(_skill_id: String) -> void:
+		# Clear this tooltip when a skill is changed (to prevent them lingering when hovered)
+		# TODO: could be handled better
+		visible = false)
+
 func _on_hovered_skill_changed(skill_info: SkillInfo) -> void:
 	if skill_info:
 		visible = true
